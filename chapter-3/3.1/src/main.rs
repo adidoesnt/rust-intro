98b4daa608c5bf5044196b6f85ae580f5f0beb90
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-01-18 22:06:42 +0800

    add code for shadowing

--- chapter-3/3.1/src/main.rs
+++ chapter-3/3.1/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // let, const and mut
     let _x: u32 = 1;
     let mut y: u32 = 2;
     println!("x: {_x}, y: {y}");
@@ -7,4 +8,22 @@ fn main() {
     y = 3;
     // z = 4; // error
     println!("x: {_x}, y: {y}");
+
+    // shadowing
+    let x: u32 = 10;
+    let x = x * 2;
+    {
+        let x = x * 2;
+        println!("x: {x}");
+    }
+    println!("x: {x}");
+
+    // shadowing can be used to change a variable's type
+    let _word = "word";
+    println!("word: {_word}");
+    let _word = _word.len();
+    println!("word: {_word}");
+    // let mut word2 = "word2";
+    // println!("word2: {word2}");
+    // word2 = word2.len(); // error
 }
