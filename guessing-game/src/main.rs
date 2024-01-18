5a5949179bef9f13ca1411e0813dd18653251eec
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-01-18 21:45:04 +0800

    add loop

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -5,17 +5,24 @@ use std::io;
 fn main() {
     let secret: u32 = thread_rng().gen_range(1..=10);
     println!("*** Guess the number ***");
-    println!("Input your guess:");
-    let mut guess: String = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please enter a number");
-    println!("You guessed {guess}");
-    match guess.cmp(&secret) {
-        Less => println!("You guessed too low..."),
-        Greater => println!("You guessed too high..."),
-        Equal => println!("Nice, you won!"),
+    loop {
+        println!("Input your guess:");
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed {guess}");
+        match guess.cmp(&secret) {
+            Less => println!("You guessed too low..."),
+            Greater => println!("You guessed too high..."),
+            Equal => {
+                println!("Nice, you won!");
+                break;
+            }
+        }
     }
-    println!("The secret number was {secret}")
 }
